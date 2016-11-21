cffe49788064c81fe65a92134addf215b11d033f
aquav_000 <aquav_000@aquavaio>
2016-11-21 13:46:26 -0700

    Formatting done.

--- empmgmt-servlet/src/main/java/com/neha/empmgmt/dao/EmployeeDao.java
+++ empmgmt-servlet/src/main/java/com/neha/empmgmt/dao/EmployeeDao.java
@@ -6,9 +6,14 @@ import com.neha.empmgmt.model.Employee;
 
 public interface EmployeeDao {
 	Employee findById(int id);
+
 	Employee findByEmail(String email);
+
 	List<Employee> findAll();
+
 	boolean save(Employee employee);
+
 	boolean deleteById(int id);
+
 	boolean update(Employee employee);
 }
--- empmgmt-servlet/src/main/java/com/neha/empmgmt/database/DatabaseConnectionFactory.java
+++ empmgmt-servlet/src/main/java/com/neha/empmgmt/database/DatabaseConnectionFactory.java
@@ -6,21 +6,22 @@ import java.sql.SQLException;
 
 public class DatabaseConnectionFactory {
 	// JDBC driver name and database URL
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://localhost/empmgmt";
 	private static Connection connection;
-	//  Database credentials
+	// Database credentials
 	static final String USER = "neha";
 	static final String PASS = "1234";
-	public static Connection getConnection(){
-		if(connection == null){
-			//STEP 2: Register JDBC driver
-		      try {
+
+	public static Connection getConnection() {
+		if (connection == null) {
+			// STEP 2: Register JDBC driver
+			try {
 				Class.forName(JDBC_DRIVER);
-			
-		      //STEP 3: Open a connection
-		      System.out.println("Connecting to database...");
-				connection = DriverManager.getConnection(DB_URL,USER,PASS);
+
+				// STEP 3: Open a connection
+				System.out.println("Connecting to database...");
+				connection = DriverManager.getConnection(DB_URL, USER, PASS);
 			} catch (ClassNotFoundException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
